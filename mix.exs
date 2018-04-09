edaf6e2e721c4bf7b3b39399e0b519f0133c411b
Rahul Garg <vihu89@gmail.com>
2018-04-09 11:48:17 -0700

    add native rust structure

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule ElixirReedSolomonErasure.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      compilers: [:rustler] ++ Mix.compilers,
+      rustler_crates: rustler_crates(),
       deps: deps()
     ]
   end
@@ -18,11 +20,18 @@ defmodule ElixirReedSolomonErasure.MixProject do
     ]
   end
 
+  def rustler_crates do
+    [reed_solomon_erasure_native: [
+      path: "native/reed_solomon_erasure_native",
+      mode: rustc_mode(Mix.env)
+    ]]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-    ]
+    [{:rustler, github: "hansihe/rustler", sparse: "rustler_mix"}]
   end
+
+  defp rustc_mode(:prod), do: :release
+  defp rustc_mode(_), do: :debug
 end
